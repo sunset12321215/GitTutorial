89d4afa9d2b4176293a70b1ecefeba97015d2e83
mac <mac@macs-MacBook-Pro.local>
2018-09-14 21:27:38 +0700

    Initial Commit

---
+++ GitTutorial/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GitTutorial
+//
+//  Created by mac on 9/14/18.
+//  Copyright © 2018 mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("Git Tutorial")
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
